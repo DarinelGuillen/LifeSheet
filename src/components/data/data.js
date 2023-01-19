eb797e49e577d19304e93d83b9317a82d1841c5c
DarinelGuillen <DarinelGuillen>
2023-01-19 16:43:30 -0600

    "[ADD] Deploy App GitHub"

---
+++ src/components/data/data.js
@@ -0,0 +1,16 @@
+export const data={
+    personal:{
+        name:"Chrstian Darinel",
+        lastName:"Escobar Guillen",
+        nickname:"Darinel E.G."
+        
+    },
+    contacto:{
+
+    },
+    habilidades:[
+        {node:""},
+        {java:""},
+        {JS:""}
+    ]
+}
