de113259d33dd5f264521ef433cba4e51cae27ea
DarinelGuillen <DarinelGuillen>
2023-01-19 17:05:29 -0600

    "[ADD] Deploy App GitHub"

--- src/components/atoms/Header.jsx
+++ src/components/atoms/Header.jsx
@@ -1,20 +1,21 @@
 import D from '../../assets/img/d.png'
 import menuD from '../../assets/img/menu.png'
-
+import {data} from '../data/data.js'
 import '../../assets/style/Header.css'
 function Header() {
-    // let menu=document.querySelector('#menu-ico')
-    // let navbar  =document.querySelector('.navbar')
-    // menu.onclick=()=>{
-    //     menu.classList.toggle('bx-x');
-    //     navbar.classList.toggle('open');
-    // }
+    //let menu=document.querySelector('#menu-icon')
+    const handlerClick = (e) => {
+    let navbar  =document.querySelector('.navbar')
+        e.target.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+        
+    }
     return ( 
         <header>
             <div>
                 <a href="#" className="LogoD">
                 <img  id="D"src={D} alt="Logo de la pagina SPA Single page aplication" />
-            <span> Darinel E.G. </span>
+            <span> {data.personal.nickname} </span>
             </a>
             </div>
             <ul className="navbar">
@@ -24,7 +25,7 @@ function Header() {
             <li><a href='#'>More</a></li>
             <li><a href='#'><button>HERE</button></a></li>
             </ul>
-            <div className='main' id="menu-icon">
+            <div className='main' id="menu-icon" onClick={handlerClick}>
                 <a href="#"><img id="D"src={menuD} alt="Foto DE MENU" /></a>
             </div>
 
