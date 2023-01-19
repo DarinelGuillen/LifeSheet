b5818cc1b5b547ccfc9f396fd90853b0b6a2a61d
DarinelGuillen <DarinelGuillen>
2023-01-18 19:15:20 -0600

    Header none finished yet JS Don´t now how to use it on it Commit

---
+++ src/components/atoms/Header.jsx
@@ -0,0 +1,35 @@
+import D from '../../assets/img/d.png'
+import menuD from '../../assets/img/menu.png'
+
+import '../../assets/style/Header.css'
+function Header() {
+    // let menu=document.querySelector('#menu-ico')
+    // let navbar  =document.querySelector('.navbar')
+    // menu.onclick=()=>{
+    //     menu.classList.toggle('bx-x');
+    //     navbar.classList.toggle('open');
+    // }
+    return ( 
+        <header>
+            <div>
+                <a href="#" className="LogoD">
+                <img  id="D"src={D} alt="Logo de la pagina SPA Single page aplication" />
+            <span> Darinel Escobar </span>
+            </a>
+            </div>
+            <ul className="navbar">
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>None</a></li>
+            <li><a href='#'>Work</a></li>
+            <li><a href='#'>More</a></li>
+            <li><a href='#'><button>HERE</button></a></li>
+            </ul>
+            <div className='main' id="menu-icon">
+                <a href="#"><img id="D"src={menuD} alt="Foto DE MENU" /></a>
+            </div>
+
+        </header>
+     );
+}
+
+export default Header;
---
+++ src/components/atoms/Nombre.jsx
@@ -0,0 +1,9 @@
+// import { process_params } from "express/lib/router";
+
+function Nombre({msn}){
+    return(
+        <h2> {msn} </h2>
+    )
+}
+
+export default Nombre;
